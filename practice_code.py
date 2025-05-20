55bb7e90ca02b9175b67a7c8add68cffca12c5fa
Aniket0701 <ashukla280@gmail.com>
2025-05-20 13:34:01 +0530

    new code

--- practice_code.py
+++ practice_code.py
@@ -1,2 +1,28 @@
+
 a = 'Hello_World'
 print (a)
+
+# First Code
+print("hello, World!")
+
+# checking Version
+import sys
+print(sys.version)
+
+# Variables & it's Type
+a = 1
+b = 2.0
+c = True
+d = 'Hello'
+
+print(type(a), type(b), type(c), type(d))
+
+x,y,z = 'Orange', 'apple','Banana'
+print(x,y,z)
+
+x = y = z = 'Apple'
+print(x,y,z)
+
+Fruits = ['Apple','Mango','Grapes']
+x,y,z = Fruits
+print(x,y,z)
