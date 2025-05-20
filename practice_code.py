54487488be42b176ec2505d68bb9888488001cdb
Aniket0701 <ashukla280@gmail.com>
2025-05-20 15:55:07 +0530

    Updated code

--- practice_code.py
+++ practice_code.py
@@ -26,3 +26,93 @@ print(x,y,z)
 Fruits = ['Apple','Mango','Grapes']
 x,y,z = Fruits
 print(x,y,z)
+
+x = 'Python is Awesome'
+print(x)
+
+x = 'python '
+y = 'is '
+z = 'Awesome'
+
+print(x+y+z)
+
+x= 5
+y = 10
+print(x+y)
+
+x = 5
+y = 'john'
+
+print(x ,y)
+
+x = 'awesome'
+
+def myfunc():
+    print('python is ' + x)
+
+myfunc()
+
+x = 50
+y = 100
+z = 150
+
+def MyFunction():
+    print(x+y+z)
+
+MyFunction()
+
+x = 'Awesome'
+
+def myfunc():
+    global x
+    x = ' fantastic'
+    print('python is' + x)
+
+myfunc()
+
+print ('python is' + x)
+
+# Numbers 
+x = range(6)
+print(type(x))
+
+
+z = 1
+a =float(z)
+
+print(type(z))
+print(type(a))
+
+import random
+print(random.randrange(1,10))
+
+#Casting
+x = int(25.10)
+y = float(10)
+z = str(1000)
+
+print(x , y , z)
+
+# String
+
+print('Hello World')
+
+a = 'Hello, World'
+print(a[:5])
+
+for x in 'banana':# String in loops
+    print(x)
+
+a = "hello"
+print(len(a)) # Length
+
+# IN
+TXT = 'The best thing in life are free'
+
+print('free' in TXT)
+
+#Not In
+
+txt = 'The best thing in life are free!'
+
+print('expensive' not in txt)
